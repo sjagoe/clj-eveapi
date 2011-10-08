8b7dc4a03fc32e8f8058c356c4fcddb753573ecb
Simon Jagoe <sjagoe@enthought.com>
2011-10-08 20:48:44 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -15,10 +15,9 @@
 
 (defproject clj-eveapi "0.0.1-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [cupboard "1.0.0-SNAPSHOT"]
-                 [clj-http "0.1.1"]
-                 [clj-time "0.1.0-RC1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-run "1.0.0"]])
+                 [clj-http "0.2.1"]
+                 [clj-time "0.3.1"]
+                 [com.sleepycat/je "4.0.92"]])
