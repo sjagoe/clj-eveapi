e50455f67727f8e9300b6bf3f01b2c4e1fd87f5e
Simon Jagoe <sjagoe@enthought.com>
2011-10-08 20:04:47 +0100

    Added license

--- project.clj
+++ project.clj
@@ -1,3 +1,18 @@
+;; EVE Manager - Character and corporation management for EVE Online
+;; Copyright (C) 2011 Simon Jagoe
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of version 3 of the GNU General Public
+;; License as published by the Free Software Foundation.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (defproject clj-eveapi "0.0.1-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
